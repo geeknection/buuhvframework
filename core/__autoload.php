ac7d7b843211c5d85c7e3b302085471cc120560f
Bruno Nascimento <bruno.nascimento.ava@gmail.com>
2020-03-29 18:01:35 -0300

    Autoload

--- core/__autoload.php
+++ core/__autoload.php
@@ -8,8 +8,8 @@ require(__DIR__ . '/config.php');
 final class AutoloadCore {
     public function __construct() {
         spl_autoload_register(array($this, 'load'));
-        spl_autoload_register(array($this, 'loadControlls'));
-        spl_autoload_register(array($this, 'loadModels'));
+        $this->loadControlls();
+        $this->loadModels();
     }
     /**
      * Carrega as classes do framework
@@ -23,17 +23,41 @@ final class AutoloadCore {
      * Carrega os controladores da aplicação
      * @return void
      */
-    private function loadControlls($className) {
-        $file = PATH . '/app/controllers/' . mb_strtolower($className) . '/index.php';
-        if (file_exists($file)) require_once($file);
+    private function loadControlls() {
+        try
+        {
+            $path   = PATH . '/app/controllers/*';
+            $dirs    = array_filter(glob($path), 'is_dir');
+            foreach ($dirs as $dir)
+            {
+                $file = $dir . '/index.php';
+                if (file_exists($file)) require_once($file);
+            }
+        }
+        catch(Exception $e)
+        {
+            echo $e->getMessage() . PHP_EOL;
+        }
     }
     /**
      * Carrega as models da aplicação
      * @return void
      */
-    private function loadModels($className) {
-        $file = PATH . '/app/models/' . mb_strtolower($className) . '/index.php';
-        if (file_exists($file)) require_once($file);
+    private function loadModels() {
+        try
+        {
+            $path   = PATH . '/app/models/*';
+            $dirs    = array_filter(glob($path), 'is_dir');
+            foreach ($dirs as $dir)
+            {
+                $file = $dir . '/index.php';
+                if (file_exists($file)) require_once($file);
+            }
+        }
+        catch(Exception $e)
+        {
+            echo $e->getMessage() . PHP_EOL;
+        }
     }
 }
 new AutoloadCore();
