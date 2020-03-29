1c15cc192e9be841a5512c585b8f3f53ec8b8c24
Bruno Nascimento <bruno.nascimento.ava@gmail.com>
2020-03-29 18:08:58 -0300

    Autoload

--- core/__autoload.php
+++ core/__autoload.php
@@ -8,8 +8,8 @@ require(__DIR__ . '/config.php');
 final class AutoloadCore {
     public function __construct() {
         spl_autoload_register(array($this, 'load'));
-        $this->loadControlls();
-        $this->loadModels();
+        spl_autoload_register(array($this, 'loadControllers'));
+        spl_autoload_register(array($this, 'loadModels'));
     }
     /**
      * Carrega as classes do framework
@@ -23,41 +23,17 @@ final class AutoloadCore {
      * Carrega os controladores da aplicação
      * @return void
      */
-    private function loadControlls() {
-        try
-        {
-            $path   = PATH . '/app/controllers/*';
-            $dirs    = array_filter(glob($path), 'is_dir');
-            foreach ($dirs as $dir)
-            {
-                $file = $dir . '/index.php';
-                if (file_exists($file)) require_once($file);
-            }
-        }
-        catch(Exception $e)
-        {
-            echo $e->getMessage() . PHP_EOL;
-        }
+    private function loadControllers($className) {
+        $file = PATH . '/app/controllers/' . mb_strtolower($className) . '/index.php';
+        if (file_exists($file)) require_once($file);
     }
     /**
      * Carrega as models da aplicação
      * @return void
      */
-    private function loadModels() {
-        try
-        {
-            $path   = PATH . '/app/models/*';
-            $dirs    = array_filter(glob($path), 'is_dir');
-            foreach ($dirs as $dir)
-            {
-                $file = $dir . '/index.php';
-                if (file_exists($file)) require_once($file);
-            }
-        }
-        catch(Exception $e)
-        {
-            echo $e->getMessage() . PHP_EOL;
-        }
+    private function loadModels($className) {
+        $file = PATH . '/app/models/' . mb_strtolower($className) . '/index.php';
+        if (file_exists($file)) require_once($file);
     }
 }
 new AutoloadCore();
