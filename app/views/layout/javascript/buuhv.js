a7e44ddf7780770f1fa3599819de9731d9e97419
Bruno Nascimento <bruno.nascimento.ava@gmail.com>
2020-04-04 10:57:02 -0300

    BuuhV Library Js

---
+++ app/views/layout/javascript/buuhv.js
@@ -0,0 +1,46 @@
+class BuuhVJS {
+    /**
+     * Executa uma ação após a página carregar completamente
+     * @param {*} callback 
+     */
+    ready(callback) {
+        window.addEventListener('load', () => {
+            try {
+                callback();
+            }
+            catch(error) {
+                console.error(error.message);
+            }
+        });
+    }
+    /**
+     * Alterna um submenenu como aberto ou fechado
+     * @param {*} event 
+     */
+    toggleMenu(event) {
+        try {
+            let parent = event.target.parentElement;
+            if (parent.classList.contains('active') === false) {
+                parent.classList.add('active');
+            } else {
+                parent.classList.remove('active');
+            }
+        }
+        catch(error) {
+            console.error(error.message);
+        }
+    }
+    /**
+     * Escuta determinado evento de um elemento
+     */
+    listener(element, event, callback) {
+        try {
+            let item = document.querySelector(element);
+            item.addEventListener(event, callback);
+        }
+        catch(error) {
+            console.error(error.message);
+        }
+    }
+}
+const BuuhVJS = new BuuhVJS();
